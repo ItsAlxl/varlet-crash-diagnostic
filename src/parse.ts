77d79059afffc1f70f06f2292f4c5fc389ae45bb
alxl <its.alxl@proton.me>
2026-08-22 20:53:26 -0500

    in case of reloads, only get most recent load order

--- src/parse.ts
+++ src/parse.ts
@@ -2,6 +2,7 @@ import { stringSimilarity } from "string-similarity-js"
 
 const rgxGuid = /[\dabcdef]{8}-[\dabcdef]{4}-[\dabcdef]{4}-[\dabcdef]{4}-[\dabcdef]{12}/
 const rgxCrashMessage = /-\s*\[([^\]]+)]: ([^-]+)\s*-/
+const rgxModLoadingStart = /\[Lua\] \[Mod\] Loading [^\n]+mod_load_order.txt/g
 const rgxModLoaded = /\[Lua\] Init DMF mod '([^']+)'/g
 
 const rgxEngineError = /<<Crash>>([\s\S]+?)<<\/Crash>>/
@@ -126,14 +127,20 @@ export function parseCrashText(text: string) {
 }
 
 export function parseLoadOrder(logText: string) {
+	// if there were reloads, we only care about the most recent
+	const starts = [...logText.matchAll(rgxModLoadingStart)]
+	const startAt = starts[starts.length - 1].index
+
 	const matches = logText.matchAll(rgxModLoaded)
 	const loadOrder: string[] = []
 	let first = true
 	for (const m of matches) {
-		const modName = m[1]
-		if (!(first && modName === "DMF"))
-			loadOrder.push(modName)
-		first = false
+		if (m.index > startAt) {
+			const modName = m[1]
+			if (!(first && modName === "DMF"))
+				loadOrder.push(modName)
+			first = false
+		}
 	}
 	return loadOrder
 }
