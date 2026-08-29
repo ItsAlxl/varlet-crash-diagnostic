878a2ab0597ead7df4dce83b1a29db88540dfc0e
alxl <its.alxl@proton.me>
2026-08-29 10:39:28 -0500

    rewrite bot response builder

--- apps/discord/src/bot.ts
+++ apps/discord/src/bot.ts
@@ -1,7 +1,19 @@
 import { configureLocalization, trMarkdown, trReport } from "@varlet-crash-diagnostic/localize/all"
 import { createLogReport, findCrashInsights, getDescTerse, type InsightResult, type LogReport } from "@varlet-crash-diagnostic/log-parse/insights"
-import { findDumpGuid, parseCrashText } from "@varlet-crash-diagnostic/log-parse/parse"
+import { findDumpGuid, parseCrashText, type ParsedCrashText } from "@varlet-crash-diagnostic/log-parse/parse"
 import { Client, EmbedBuilder, Events, GatewayIntentBits, MessageReferenceType, type APIEmbedField, type Message, type MessageReplyOptions, type MessageSnapshot, type PartialMessage } from "discord.js"
+
+type AttachmentReport = {
+	fileName: string,
+	report: LogReport,
+}
+
+type ResponseComponents = {
+	isPing: boolean,
+	crashTextParse?: ParsedCrashText,
+	reports?: AttachmentReport[],
+	dumpGuids?: string[],
+}
 
 const replyRetargeting = process.env.REPLY_RETARGETING?.toLowerCase() === "true"
 const autoAskForLogs = process.env.AUTO_ASK_FOR_LOGS?.toLowerCase() === "true"
@@ -62,7 +74,7 @@ function embedFieldsFromInsights(insights: InsightResult[]): APIEmbedField[] {
 	}).filter(f => f.value.length > 0)
 }
 
-function msgPingsMe(msg: Message<boolean> | PartialMessage<boolean> | MessageSnapshot) {
+function msgPingsMe(msg: Message | PartialMessage | MessageSnapshot) {
 	return (client.user && msg.mentions.members?.has(client.user.id)) ?? false
 }
 
@@ -70,37 +82,27 @@ function getLogSuffix(guid: string) {
 	return "-" + guid + ".log"
 }
 
-async function sendReportFrom(msg: Message<boolean> | MessageSnapshot, replyRetargeted = false, replyTo: Message<boolean> | undefined = undefined) {
-	const response: MessageReplyOptions = {}
+async function parseMessage(msg: Message | MessageSnapshot, replyRetargeted = false) {
+	const components: ResponseComponents = {
+		isPing: msgPingsMe(msg) || replyRetargeted,
+		crashTextParse: parseCrashText(msg.content),
+	}
 
-	const isPing = msgPingsMe(msg) || replyRetargeted
-	const crashTextParse = parseCrashText(msg.content)
-	const hasCrashReport = crashTextParse !== undefined
-	let reportMismatch = hasCrashReport
-
+	const attachments = msg.attachments.values()
 	const dumpGuids: string[] = []
-	const logGuids: string[] = []
-	const reports: ({ fileName: string, report: LogReport })[] = []
-	for (const attach of msg.attachments.values()) {
+	const reports: AttachmentReport[] = []
+	for (const attach of attachments) {
 		const fileName = attach.name
 		if (fileName.endsWith(".txt") || fileName.endsWith(".log")) {
 			const fileFetch = await fetch(attach.url)
 			if (fileFetch.ok) {
 				const logReport = createLogReport(await fileFetch.text(), fileName)
 				const logGuid = logReport.guid
-				logGuids.push(logGuid)
-				if (!isDupe(dedupeHistoryLogs, logGuid)) {
-					if (isPing)
-						rememberDupe(dedupeHistoryLogs, logGuid)
-
+				if (!reports.some(r => r.report.guid === logGuid))
 					reports.push({
 						fileName: "varlet-" + logGuid,
 						report: logReport
 					})
-
-					if (hasCrashReport && reportMismatch)
-						reportMismatch = logGuid !== crashTextParse.guid
-				}
 			}
 		} else {
 			const dumpGuid = findDumpGuid(fileName)
@@ -108,45 +110,77 @@ async function sendReportFrom(msg: Message<boolean> | MessageSnapshot, replyReta
 				dumpGuids.push(dumpGuid)
 		}
 	}
-	const unmatchedDumpGuids = dumpGuids.filter(guid => logGuids.findIndex(g => g === guid) < 0)
 
-	let containsContent = false
+	if (reports.length > 0)
+		components.reports = reports
+	if (dumpGuids.length > 0)
+		components.dumpGuids = dumpGuids
+
+	return components
+}
+
+async function sendReportFrom(msg: Message | MessageSnapshot, replyRetargeted = false, replyTo: Message | undefined = undefined) {
+	const response: MessageReplyOptions = {}
+
+	const components = await parseMessage(msg, replyRetargeted)
+	const reports = components.reports
+	const crashTextParse = components.crashTextParse
+	const isPing = components.isPing
+
 	const embedBuilder = new EmbedBuilder()
-	if (unmatchedDumpGuids.length > 0) {
-		containsContent ||= autoAskForLogs && reports.length === 0 || isPing
-		embedBuilder.addFields(reports.length > 0
-			? {
-				name: trReport("insight_guid_dumpfile_mismatch_title"),
-				value: trReport("insight_guid_dumpfile_mismatch_desc")
+
+	if (components.dumpGuids) {
+		if (reports) {
+			if (isPing && components.dumpGuids.some(guid => reports.some(r => r.report.guid === guid))) {
+				embedBuilder.addFields({
+					name: trReport("insight_guid_dumpfile_mismatch_title"),
+					value: trReport("insight_guid_dumpfile_mismatch_desc")
+				})
 			}
-			: {
+		} else if (autoAskForLogs) {
+			embedBuilder.addFields({
 				name: trReport("insight_guid_dumpfile_useless_title"),
 				value: trReport("insight_guid_dumpfile_useless_desc")
-			}
-		)
+			})
+		}
 	}
 
-	if (reports.length > 0) {
-		containsContent ||= isPing
-
-		if (reportMismatch)
+	let respondToCrashText = true
+	if (reports && isPing) {
+		if (crashTextParse && !reports.some(r => r.report.guid === crashTextParse.guid)) {
 			embedBuilder.addFields({
 				name: trReport("insight_guid_mismatch_title"),
 				value: trReport(reports.length === 1 ? "insight_guid_mismatch_desc" : "insight_guid_mismatch_desc_pl")
 			})
-		if (reports.length === 1)
-			embedBuilder.addFields(...embedFieldsFromInsights(reports[0].report.insights))
+		}
 
-		response.files = reports.map(r => {
-			return {
-				attachment: Buffer.from(r.report.reportText),
-				name: r.fileName + ".txt",
-				title: r.fileName
+		const freshReports: AttachmentReport[] = []
+		for (const r of reports) {
+			if (!isDupe(dedupeHistoryLogs, r.report.guid))
+				freshReports.push(r)
+		}
+
+		if (freshReports.length > 0) {
+			respondToCrashText = false
+
+			if (freshReports.length === 1)
+				embedBuilder.addFields(...embedFieldsFromInsights(freshReports[0].report.insights))
+
+			for (const r of freshReports) {
+				rememberDupe(dedupeHistoryLogs, r.report.guid)
 			}
-		})
-	} else if (crashTextParse && (autoAskForLogs || isPing) && observeDedupe(dedupeHistoryPastes, crashTextParse.guid)) {
-		containsContent ||= true
 
+			response.files = freshReports.map(r => {
+				return {
+					attachment: Buffer.from(r.report.reportText),
+					name: r.fileName + ".txt",
+					title: r.fileName
+				}
+			})
+		}
+	}
+
+	if (respondToCrashText && crashTextParse && (autoAskForLogs || isPing) && !isDupe(dedupeHistoryPastes, crashTextParse.guid)) {
 		embedBuilder.addFields(embedFieldsFromInsights(findCrashInsights(crashTextParse, true)))
 		embedBuilder.setDescription(`${trMarkdown("faq_log_location_bot_prefix", undefined, "en")}
 \`${trMarkdown("faq_log_location_steam_path", undefined, "en")}\`
@@ -154,7 +188,7 @@ async function sendReportFrom(msg: Message<boolean> | MessageSnapshot, replyReta
 ${trMarkdown("faq_log_location_bot_suffix", { logNameEnd: getLogSuffix(crashTextParse.guid) }, "en")}`)
 	}
 
-	if (containsContent) {
+	if (embedBuilder.length > 0 || (response.files?.length ?? 0) > 0) {
 		embedBuilder.setURL("https://itsalxl.github.io/varlet-crash-diagnostic")
 		embedBuilder.setTitle(trReport("varlet_tool_title"))
 		embedBuilder.setColor("#782312")
